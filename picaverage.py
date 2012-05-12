541b34d9697a341c04f8bfe2cba3e346da0abb74
Seiei Higa <hanachin@gmail.com>
2012-05-12 11:41:19 +0900

    fastergsgs

--- picaverage.py
+++ picaverage.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python
 # coding: utf-8
 import Image
-import numpy as np
+from operator import add
 
 def picaverage(x, y, im):
   w, h = im.size
   if x < 0 or y < 0 or w < x or h < y or im.mode != 'RGB':
     raise Exception('argument error')
 
-  pixels = [im.getpixel((x + i, y + j))
-            for i in range(-2, 3)
-            for j in range(-2, 3)
-            if 0 <= x+i < w and 0 <= y+j < h]
+  left, upper, right, lower = x - 2, y - 2, x + 2, y + 2
 
-  return tuple(map(int, map(np.average, zip(*pixels))))
+  if left < 0:
+    left = 0
+  if upper < 0:
+    upper = 0
+  if right > w:
+    right = w
+  if lower > h:
+    lower = h
+
+  totalpixels = [0, 0, 0]  
+  pixels = im.crop((left, upper, right, lower)).getdata()
+  for pixel in pixels:
+    totalpixels[0] += pixel[0]
+    totalpixels[1] += pixel[1]
+    totalpixels[2] += pixel[2]
+
+  l = len(pixels)
+  totalpixels[0] /= l
+  totalpixels[1] /= l
+  totalpixels[2] /= l
+
+  return tuple(totalpixels)
